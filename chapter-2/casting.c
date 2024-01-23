c38d22c1fb2cb26001a48aab0313377053375828
Chris Booty <chrisalan313@gmail.com>
2024-01-22 21:53:17 -0500

    Fix missing return type on main

--- chapter-2/casting.c
+++ chapter-2/casting.c
@@ -5,23 +5,23 @@ typedef unsigned char *byte_pointer;
 void show_bytes(byte_pointer start, size_t len) {
     size_t i;
     for (i = 0; i < len; i++)
-        printf("%.2x", start[i]); 
+        printf("%.2x", start[i]);
     printf("\n");
 }
 
 void show_int(int x) {
-    show_bytes((byte_pointer) &x, sizeof(int)); 
+    show_bytes((byte_pointer) &x, sizeof(int));
 }
 
 void show_float(float x) {
-    show_bytes((byte_pointer) &x, sizeof(float)); 
+    show_bytes((byte_pointer) &x, sizeof(float));
 }
 
 void show_pointer(void *x) {
-    show_bytes((byte_pointer) &x, sizeof(void *)); 
+    show_bytes((byte_pointer) &x, sizeof(void *));
 }
 
-main() {
+int main() {
     int i = 12345;
     float f = (float) i;
     int *p = &i;
