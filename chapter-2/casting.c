4e7ae61dec8681b5108e386de6c6ef47d1dfc195
Chris Booty <chrisalan313@gmail.com>
2024-01-22 21:25:12 -0500

    Add casting example to view bytes:

---
+++ chapter-2/casting.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    size_t i;
+    for (i = 0; i < len; i++)
+        printf("%.2x", start[i]); 
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int)); 
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float)); 
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *)); 
+}
+
+main() {
+    int i = 12345;
+    float f = (float) i;
+    int *p = &i;
+    show_int(i);
+    show_float(f);
+    show_pointer(p);
+}
